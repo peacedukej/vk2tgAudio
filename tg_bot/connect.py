ae80721c236d0dcc3abe9e57d9d1330f4d0946fa
peacedukej <lovecoldreams@gmail.com>
2024-08-09 11:20:13 +0300

    rabbitmq added

--- tg_bot/connect.py
+++ tg_bot/connect.py
@@ -2,23 +2,26 @@ import asyncio
 import logging
 import sys
 import aiohttp
+import aio_pika
+import json 
 
 from aiogram import Bot, Dispatcher, html
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 
-from config import tg_token
+from config import tg_token, RABBITMQ_URL
 from tg_bot.generate_token import generate_hex_token
 
 TOKEN = tg_token
 API_URL = "http://localhost:8000/"
+#RABBITMQ_URL = "amqp://guest:guest@localhost/"
 
 routers = [
     'new_telegram_user'
 ]
-
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 
@@ -46,15 +49,42 @@ async def command_start_handler(message: Message) -> None:
 async def echo_handler(message: Message) -> None:
     await message.answer("Бот не принимает сообщений.")
 
+async def rabbitmq_listener(loop):
+    connection = await aio_pika.connect_robust(RABBITMQ_URL, loop=loop)
+    channel = await connection.channel()
+
+    queue = await channel.declare_queue("telegram_queue", auto_delete=True)
+
+    async with queue.iterator() as queue_iter:
+        async for message in queue_iter:
+            async with message.process():
+                request = json.loads(message.body)
+
+                print(request)
+                print(type(request))
+                #valid_json = json.load(message.body.decode())
+                #print(valid_json)
+                tg_id = request['tg_id']
+                #print(tg_id)
+                #await bot.send_message(tg_id, "test message")
+                audiofile = FSInputFile(request['filename'])
+                await bot.send_audio(chat_id=tg_id, audio=audiofile)
 
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-
+    # Запуск прослушивания RabbitMQ в отдельной задаче
+    loop = asyncio.get_running_loop()
+    rabbitmq_task = loop.create_task(rabbitmq_listener(loop))
+    
     # And the run events dispatching
     await dp.start_polling(bot)
 
+
+
+    # Ожидание завершения задачи RabbitMQ
+    await rabbitmq_task
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO, stream=sys.stdout)
--- vk_bot/connect.py
+++ vk_bot/connect.py
@@ -10,10 +10,6 @@ from config import vk_token
 
 API_URL = "http://localhost:8000/"
 
-routers = [
-    'new_vk_user'
-]
-
 vk_session = vk_api.VkApi(token=vk_token)
 longpoll = VkLongPoll(vk_session)
 
@@ -23,48 +19,114 @@ async def write_msg(user_id: int, message: str):
     vk_session.method('messages.send', {
         'user_id': user_id,
         'message': message,
-        'random_id': random_id  # Генерация случайного ID
+        'random_id': random_id 
     })
 
-async def is_valid_hex_token(user_token: str, expected_length: int = 32) -> bool:
-    if len(user_token) != expected_length or len(user_token) % 2 != 0:
+async def is_valid_hex_token(token: str, expected_length: int = 32) -> bool:
+    if len(token) != expected_length or len(token) % 2 != 0:
         return False
-    
-    # Регулярное выражение для проверки 16-ричных символов
     hex_pattern = re.compile(r'^[0-9a-fA-F]+$')
-    
-    # Проверка на соответствие паттерну
-    return bool(hex_pattern.match(user_token))
+    return bool(hex_pattern.match(token))
+
+
+async def get_vk_profile_info(vk_id: int) -> dict:
+    response = requests.get(url=f"https://api.vk.com/method/users.get?user_ids={vk_id}&access_token={vk_token}&v={5.199}")
+    user_get = response.json()
+    user_get = user_get['response'][0]
+    profile_data = {"vk_name": user_get['first_name'], "vk_surname": user_get['last_name']}
+    return profile_data
+
+async def get_audio_data(message_id: int) -> dict:
+    response = requests.get(url=f"https://api.vk.com/method/messages.getById?message_ids={message_id}&access_token={vk_token}&v={5.199}")
+    audio_data = response.json()["response"]["items"][0]["attachments"][0]["audio"]
+    #audio_data = audio_data["response"]["items"][0]["attachments"][0]["audio"]["url"]
+    url = audio_data["url"]
+    filename = (url.split("/")[-1]).split("?")[0]
+    print(filename)
+    return {"url": url, "filename": filename}
+
+async def download_mp3(url, filename):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                with open("music/"+filename, 'wb') as f:
+                    while True:
+                        chunk = await response.content.read(1024)
+                        if not chunk:
+                            break
+                        f.write(chunk)
+                print(f"Downloaded {filename}")
+            else:
+                print(f"Failed to download {url}, status code: {response.status}")
+
+async def db_response(type: str, router: str, data: dict) -> dict:
+    try:
+        async with aiohttp.ClientSession() as session:
+            if type == 'post':
+                async with session.post(API_URL + router, json=data) as response:
+                    response_data = await response.json()
+                    return response_data
+            elif type == 'get':
+                async with session.get(API_URL + router, params=data) as response:
+                    response_data = await response.json()
+                    return response_data
+            else:
+                raise ValueError("Unsupported request type: {}".format(type))
+    except aiohttp.ClientError as e:
+        print(f"HTTP request failed: {e}")
+        return None
+    except ValueError as e:
+        print(f"Error: {e}")
+        return None
 
 async def main() -> None:
-    print("start_polling")
     for event in longpoll.listen():
-        if event.type == VkEventType.MESSAGE_NEW:
-            if event.to_me:
-                '''
-                в БД добавить флаг, что токен выдан или нет, в зависимости от этого поменять логику
-                Добавить проверку на тип сообщения
-                '''
-                if await is_valid_hex_token(user_token=event.text):
-                    id = event.user_id
-                    response = requests.post(url=f"https://api.vk.com/method/users.get?user_ids={id}&access_token={vk_token}&v={5.199}")
-                    user_get = response.json()
-                    user_get = user_get['response'][0]
-
-                    payload = {"token": event.text, "vk_id": event.user_id, "vk_name": user_get['first_name'], "vk_surname": user_get['last_name']}
-                    async with aiohttp.ClientSession() as session:
-                        async with session.post(API_URL + routers[0], json=payload) as response:
-                            print("Response STATUS", response.status)
-                            if response.status == 200:
-                                await write_msg(event.user_id, "Успешно привязано")
-                            if response.status == 404:
-                                await write_msg(event.user_id, "Токен не найден")
-                elif 30 < len(event.text) <= 34 and ' ' not in event.text:
-                    await write_msg(event.user_id, "Токен не найден. Проверьте правильноcть написания токена.")
-                else:
-                    await write_msg(event.user_id, "Бот не отвечает на текстовые сообщения. Просто присылай мне аудиозаписи, и я перешлю их тебе в Telegram.")
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            #if event.to_me:
+            id = event.user_id
+            message_id = event.message_id
+            print(message_id)
+            if await is_valid_hex_token(token=event.text):
+                """
+                Пользователь прислал токен (по формату), проверяем наличие токена в БД
+                """
+                validate_token = await db_response(type='get', router='validate_token', data={'token': event.text})
+                if validate_token["response_status"] == 200:
+                    """
+                    Если токен корректен и найден, то заносим данные пользователя в БД
+                    """
                     
-                
+                    payload = await get_vk_profile_info(vk_id=id)
+                    payload['token'] = event.text
+                    payload['vk_id'] = id
+
+                    response = await db_response(type='post', router='new_vk_user', data=payload)
+                    if response['response_status'] == 200:
+                        await write_msg(event.user_id, "Успешно привязано. Теперь просто отправляй мне музыку.")
+                    else:
+                        await write_msg(event.user_id, "Произошла ошибка c привязкой.")
+                else:
+                    await write_msg(event.user_id, "Токен не найден.") 
+            elif event.attachments:
+                """
+                Если пользователь прислал аудио
+                """
+                validate_tg_id = await db_response(type='get', router='validate_tg_id', data={'vk_id': id})
+                if validate_tg_id['response_status'] == 200:
+                    """
+                    и если у него связаны аккаунты (для текущего vk_id есть tg_id)
+                    """
+                    if event.attachments["attach1_type"] == "audio":
+                        await write_msg(event.user_id, "Начинаю загрузку!")
+                        audio_data = await get_audio_data(message_id=message_id)
+                        if audio_data:
+                            await download_mp3(url=audio_data["url"], filename=audio_data["filename"])
+                            await db_response(type='post', router='send_audio', data={"vk_id": id, "filename": audio_data["filename"]})
+                        await write_msg(event.user_id, "Загружено!")
+                else:
+                    await write_msg(event.user_id, "Ты прислал аудио, но ты еще не связал аккаунты.")
+            else:
+                await write_msg(event.user_id, "Я пока не умею обрабатывать другие текстовые запросы.")
 
 if __name__ == "__main__":
     asyncio.run(main())
